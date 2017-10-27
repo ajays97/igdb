c6fb9150e6532f51adee17a10458b7c9fad0e33a
Ajay Srinivas <ajay.s1997@gmail.com>
2017-10-27 20:22:42 +0530

    Add date splicing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const Handlebars = require('handlebars');
 const bodyParser = require('body-parser');
 var conn = require('./dbconnect');
 const url = require('url');
@@ -12,6 +13,10 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+Handlebars.registerHelper('getYear', function (dateString) {
+    return new Handlebars.SafeString(dateString.getYear() + 1900);
+});
 
 app.get('/', function (req, res) {
     const sql = "SELECT * FROM games_master;";
