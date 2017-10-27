67afa96d47b3edd3285cfb7c73e3b84bb9c89a22
Ajay Srinivas <ajay.s1997@gmail.com>
2017-10-27 19:57:41 +0530

    Nothign

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', function (req, res) {
     const sql = "SELECT * FROM games_master;";
     conn.query(sql, function (err, games, fields) {
-        console.log(games[0].title);
         res.render('index', {games: games});
     });
 
