a8c6120f5d85a1c0c412cb26392c5e6f9248d6b6
Ajay Srinivas <ajay.s1997@gmail.com>
2017-10-29 10:08:41 +0530

    Final Deployable to EBS v4

--- session.js
+++
@@ -1,8 +0,0 @@
-const options = {
-    host: "igdb.ctk3p7qef1xl.us-east-1.rds.amazonaws.com",
-    user: "ajays",
-    password: "ajays1997",
-    database: "igdb_sessions"
-};
-
-module.exports = options;
